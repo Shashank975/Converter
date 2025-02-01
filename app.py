057a8e2bf326d8c30b992398d16de09956c13638
Shashank Chhoker <shashanksaad07@gmail.com>
2025-02-01 19:45:45 +0530

    add gunicorn

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# import sys
+# sys.dont_write_bytecode = True
+
+
+# import controller.controller 
+# import model.model
+# import os 
+
+
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 import sys
 sys.dont_write_bytecode = True
 
-
-import controller.controller 
+import controller.controller
 import model.model
-import os 
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=False)
