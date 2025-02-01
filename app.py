f3521897927fca78ca6a8a1936c496409fa02018
Shashank Chhoker <shashanksaad07@gmail.com>
2025-02-01 18:55:01 +0530

    update readme

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+
 import sys
 sys.dont_write_bytecode = True
-import os 
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
 import controller.controller 
 import model.model
+import os 
